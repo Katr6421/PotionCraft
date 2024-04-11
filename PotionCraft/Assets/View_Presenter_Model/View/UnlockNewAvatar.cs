8e8ee9f7b523a82b53847184cf937aaf0110d0da
TuridHojgaard <tuho@itu.dk>
2024-04-11 13:35:45 +0200

    Færdig med minigames

--- PotionCraft/Assets/View_Presenter_Model/View/UnlockNewAvatar.cs
+++ PotionCraft/Assets/View_Presenter_Model/View/UnlockNewAvatar.cs
@@ -4,12 +4,14 @@ using UnityEngine.UI;
 
 public class UnlockNewAvatar : MonoBehaviour
 {
-    [SerializeField] private Sprite[] _colors;
+    [SerializeField] private GameObject _popUp;
     [SerializeField] private GameObject _newAvatar;
+    private LevelManager _levelManager;
     private float delayBeforeShowing = 1.0f; // Seconds to wait before showing the popup
 
     void Start()
     {
+        _levelManager = LevelManager.Instance;
     }
 
     // Call this method when the correct answer is chosen
@@ -23,8 +25,15 @@ public class UnlockNewAvatar : MonoBehaviour
         // Wait for the specified delay
         yield return new WaitForSeconds(delayBeforeShowing);
 
+        // Change the sprite of the new avatar
+        if (_levelManager.CurrentLevelIndex == 4) {
+            _newAvatar.GetComponent<Image>().sprite = _levelManager.GetAvatarSprite(1);
+        } else {
+            _newAvatar.GetComponent<Image>().sprite = _levelManager.GetAvatarSprite(2);
+        }
+
         // Show the popup
-        _newAvatar.SetActive(true);
+        _popUp.SetActive(true);
     
     }
 }
