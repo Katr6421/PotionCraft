cb5f08fa507f909b3730a7e4a9b8d326eb04c575
TuridHojgaard <tuho@itu.dk>
2024-04-14 17:28:35 +0200

    Tilføjet quit knap

---
+++ PotionCraft/Assets/View_Presenter_Model/View/QuitGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        // Debug.Log is used to show the message in Unity Editor's console
+        Debug.Log("Exit Game called");
+
+        // Application.Quit() does not work in the editor, so it's commented out here
+        // Uncomment this line when building the game
+        // Application.Quit();
+        
+        #if UNITY_EDITOR
+        // This line is for exiting the game in the Unity Editor.
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
