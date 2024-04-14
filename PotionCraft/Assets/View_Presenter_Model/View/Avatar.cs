fe232fd8531f1d3bec8dbb2798c9b15fbd4473be
TuridHojgaard <tuho@itu.dk>
2024-04-14 20:42:03 +0200

    Tilføjet taleboble i level 5

--- PotionCraft/Assets/View_Presenter_Model/View/Avatar.cs
+++ PotionCraft/Assets/View_Presenter_Model/View/Avatar.cs
@@ -4,15 +4,21 @@ public class Avatar : MonoBehaviour
 {
     [SerializeField] private GameObject _chooseColorBubble;
     private LevelManager _levelManager;
+    private ClickMe _clickMeBobble;
 
 
     void Start()
     {
         _levelManager = LevelManager.Instance;
+        _clickMeBobble = FindObjectOfType<ClickMe>();
         GetComponent<SpriteRenderer>().sprite = _levelManager.CurrentAvatarSprite;
     }
 
     public void OnMouseDown() {
+        if (_levelManager.CurrentLevelIndex == 5) {
+            _clickMeBobble.clickCount++;
+        }
+
         if (_chooseColorBubble.activeSelf) {
             _chooseColorBubble.SetActive(false);
         } else {
---
+++ PotionCraft/Assets/View_Presenter_Model/View/ClickMe.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMe : MonoBehaviour
+{
+    [SerializeField] private GameObject _clickMeBobble;
+    public int clickCount { get; set; } = 0;
+    
+    
+    void Update()
+    {
+        if (clickCount == 1) {
+            _clickMeBobble.SetActive(false);
+        }
+    }
+}
